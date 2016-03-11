0568da35d6542fe69391f52c1ff2fafc8f95affd
Matthew Clifford-Smith <dark.arc@gmail.com>
2016-03-11 17:37:47 +0000

    day12 fully complete

--- day12/day12.rb
+++ day12/day12.rb
@@ -1,3 +1,20 @@
+require 'json'
 $input = File.open('day12.in').read
 
 puts $input.scan(/(-*\d*)/).reduce(0){|sum,cur| sum += cur.first.to_i}
+
+hash = JSON.parse($input)
+
+def find_numbers(input)
+	case input
+	when Array
+		input.flat_map {|value| find_numbers(value)}.compact
+	when Hash
+		find_numbers(input.values).compact unless 
+			input.values.include? 'red'
+	when Fixnum
+		[input]
+	end
+end
+
+puts find_numbers(hash).inject(:+)

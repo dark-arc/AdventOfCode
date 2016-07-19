30d4024abefa8ff0174fde525e02abf17b9db640
Matthew Bayliss <dark.arc@gmail.com>
2016-07-19 22:38:20 +0100

    Added CPP for day1-2

--- day01/day01.cpp
+++ day01/day01.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 int main()
 {
-  register signed int count = 0;
+  register signed int dws = 0;
+  register signed int ups = 0;
   register char cmd;
 
-  while(cin.get(cmd)){    
-    if(cmd == '('){ count++; }
-    if(cmd == ')'){ count--; }
+  while(cin.get(cmd)){
+    if(cmd == '('){ ups++; }
+    if(cmd == ')'){ dws++; }
+    if(ups-dws == -1){cout << "B1: " << (ups+dws) << endl; }
   }
-  cout << endl << "Floor: " << count << endl;
+  cout << "Floor: " << (ups-dws) << endl;
   
   return 0;
 }
